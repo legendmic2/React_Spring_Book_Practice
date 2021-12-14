e2d96cb4896eb234afd1b64e51ce01691a01e481
legendmic2 <goonoo0731@gmail.com>
2021-12-14 18:48:08 +0900

    Initial commit

---
+++ src/main/java/com/example/react_spring_book_practice/ReactSpringBookPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.react_spring_book_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBookPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactSpringBookPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/react_spring_book_practice/ReactSpringBookPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.react_spring_book_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBookPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
